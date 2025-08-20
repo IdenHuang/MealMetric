40ffa9ee000ea3adb4e5295648a4d7ecf19bb61f
IdenHuang <idenhuang@hotmail.com>
2025-08-20 12:02:31 -0700

    Added routes to interact with recipe collection in database

---
+++ backend/recipeRoutes.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const database = require("./connect")
+const ObjectId = require("mongodb").ObjectId
+
+let recipeRoutes = express.Router()
+
+// Gets all recipe in db
+recipeRoutes.route("/recipes").get(async (request, response) => {
+    let db = database.getDb()
+    let data = await db.collection("recipe").find({}).toArray()
+    if (data.length > 0) {
+        response.json(data)
+    } else {
+        throw new Error("data not found")
+    }
+})
+
+// Gets recipe in db
+recipeRoutes.route("/recipes/:id").get(async (request, response) => {
+    let db = database.getDb()
+    let data = await db.collection("recipe").findOne({_id: new ObjectId(request.params.id)})
+    if (Object.keys(data).length > 0) {
+        response.json(data)
+    } else {
+        throw new Error("data not found")
+    }
+})
+
+// Adds recipe in db
+recipeRoutes.route("/recipe").post(async (request, response) => {
+    let db = database.getDb()
+    let mongoObject = {
+        title: request.body.title,
+        calories: request.body.calories,
+        protein: request.body.protein,
+        fat: request.body.fat,
+        carbs: request.body.carbs
+    }
+    let data = await db.collection("recipe").insertOne(mongoObject)
+    response.json(data)
+})
+
+// Updates recipes in db
+recipeRoutes.route("/recipes/id").put(async (request, response) => {
+    let db = database.getDb()
+    let mongoObject = {
+        $set: {
+            title: request.body.title,
+            calories: request.body.calories,
+            protein: request.body.protein,
+            fat: request.body.fat,
+            carbs: request.body.carbs
+        }
+    }
+    let data = await db.collection("recipe").updateOne({_id: new ObjectId(request.params.id)}, mongoObject)
+    response.json(data)
+})
+
+// Deletes recipe in db
+recipeRoutes.route("/recipes/:id").delete(async (request, response) => {
+    let db = database.getDb()
+    let data = await db.collection("recipe").deleteOne({_id: new ObjectId(request.params.id)})
+    reponse.json(data)
+})
+
+module.exports = recipeRoutes
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const connect = require("./connect")
 const express = require("express")
 const cors = require("cors")
+const recipes = require("./recipeRoutes")
 
 const app = express()
 const PORT = 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(recipes)
 
 app.listen(PORT, () => {
     connect.connectToServer()
